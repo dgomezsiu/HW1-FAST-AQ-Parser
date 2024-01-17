292f9a44b8f3c81efb7af4294835f2bbfa201bdf
David Gomez Siu <dgomezsiu@berkeley.edu>
2024-01-17 02:00:27 -0800

    transcribe_and_reverse_transcribe

--- seqparser/seq.py
+++ seqparser/seq.py
@@ -10,13 +10,19 @@ def transcribe(seq: str, reverse: bool = False) -> str:
     Write a function that will transcribe (replace DNA sequence to RNA
     by replacing all 'T' to 'U') in an input sequence
     """
-    pass
+
+    # make sure DNA sequence has only allowed nucleotides
+    assert all(nucleotide in ALLOWED_NUC for nucleotide in seq), "Input string contains unallowed nucleotides."
+
+    # iterate through sequence and transcribe
+    rna_seq = ''
+    rna_seq = rna_seq.join(TRANSCRIPTION_MAPPING[nucleotide] for nucleotide in seq)
+    return rna_seq
 
 def reverse_transcribe(seq: str) -> str:
     """
     Write a function that will transcribe an input sequence and reverse
     the sequence
     """
-    # Hey this is my comment
-    # Again!
-    pass
+
+    return transcribe(seq)[::1]
